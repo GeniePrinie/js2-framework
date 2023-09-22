134f42712f07e8fcec957296c05459f413bdd2aa
GeniePrinie <geniexpn@gmail.com>
2023-09-22 11:06:26 +0200

    Add app shell

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import RouterPathway from "./components/Global/Router";
+
+function App() {
+  return (
+    <div>
+      <RouterPathway />
+    </div>
+  );
+}
+
+export default App;
