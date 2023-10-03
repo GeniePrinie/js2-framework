bbf727548db679f7ad15f40dbc6e0b11843c39b2
GeniePrinie <geniexpn@gmail.com>
2023-10-03 23:40:44 +0200

    Remove unnecessary files

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,37 @@
 import React from "react";
-import ProductsDisplay from "./ProductsDisplayComponents/ProductsDisplay";
+import { Link } from "react-router-dom";
+import PriceDisplay from "./Global/Price";
 
 export const Products = ({ data }) => {
   return (
     <div className="container">
       <div className="card-container row justify-content-center gy-4">
         {data.map((product) => (
-          <ProductsDisplay key={product.id} product={product} />
+          <Link
+            key={product.id}
+            to={`/product/${product.id}`}
+            className="card-link text-decoration-none col-md-6 col-lg-4 col-xl-3 mb-4"
+          >
+            <div
+              className="card border-0 shadow-sm"
+              style={{ width: "100%", height: "100%" }}
+            >
+              <img
+                src={product.imageUrl}
+                className="card-img-top "
+                alt={product.title}
+                style={{ width: "100%", height: "300px", objectFit: "cover" }}
+              />
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title text-secondary">{product.title}</h5>
+                <p className="card-text text-secondary">
+                  {product.description}
+                </p>
+
+                <PriceDisplay product={product} />
+              </div>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
