51619b7f7ffd9cc757e59199b1d4a22dfe5e8f88
GeniePrinie <geniexpn@gmail.com>
2023-10-07 12:21:20 +0200

    Add search functionality

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import { Products } from "../Components/Common/Products";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 export function HomePage() {
+  const [search, setSearch] = useState("");
   const [products, setProducts] = useState([]);
   const url = "https://api.noroff.dev/api/v1/online-shop/";
 
@@ -18,6 +21,12 @@ export function HomePage() {
     fetchData();
   }, []);
 
+  let filteredProducts = products.filter((product) => {
+    return search.toLowerCase() === ""
+      ? product
+      : product.title.toLowerCase().includes(search);
+  });
+
   return (
     <div>
       <div className="container">
@@ -26,15 +35,17 @@ export function HomePage() {
           Need Help? Search bar is right below, or contact us in the Contact
           page. We hope you find what you're looking for. Good Luck!
         </p>
+        <Form>
+          <InputGroup className="my-3">
+            <Form.Control
+              onChange={(event) => setSearch(event.target.value)}
+              placeholder="Type to search..."
+            />
+          </InputGroup>
+        </Form>
       </div>
-
       <div>
-        {/* <SearchBar setResults={setResults} />
-        {results.length === 0 ? ( */}
-        <Products data={products} />
-        {/* ) : (
-          <SearchResultsList results={results} />
-        )} */}
+        <Products data={filteredProducts} />
       </div>
     </div>
   );
