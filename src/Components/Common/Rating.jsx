3f78647d93b0e503366fc8fe12bba4c44952cd22
GeniePrinie <geniexpn@gmail.com>
2023-10-07 22:11:53 +0200

    Update review's bg-color, remove (numberOfReview) in Rating.jsx

--- src/Components/Common/Rating.jsx
+++ src/Components/Common/Rating.jsx
@@ -5,10 +5,5 @@ export const Rating = ({ product }) => {
     product.rating !== null && product.rating !== undefined
       ? product.rating
       : 0;
-  const numberOfReviews = product.reviews ? product.reviews.length : 0;
-  return (
-    <p className="fs-5">
-      Rating: {rating} ({numberOfReviews})
-    </p>
-  );
+  return <p className="fs-5">Rating: {rating}/5</p>;
 };
