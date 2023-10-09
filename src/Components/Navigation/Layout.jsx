57362da67ab478d70fed758d7295a86d0781e829
GeniePrinie <geniexpn@gmail.com>
2023-10-09 12:13:40 +0200

    Add documentation in Navigation

--- src/Components/Navigation/Layout.jsx
+++ src/Components/Navigation/Layout.jsx
@@ -2,6 +2,12 @@ import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 
+/**
+ * Layout component serves as the overall layout structure for the entire application.
+ * It includes the header, the main content (determined by the router's `Outlet`), and the footer.
+ * @component
+ * @returns {JSX.Element} The Layout component.
+ */
 export function Layout() {
   return (
     <div>
